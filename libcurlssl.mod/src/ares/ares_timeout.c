ca715f665e724f7242811c1cc6ca77c2f1a04e37
woollybah <woollybah@b77917fb-863c-0410-9a43-030a88dac9d3>
2007-10-15 14:31:35 +0000

    libcurlssl initial commit. (part 5).
    git-svn-id: http://maxmods.googlecode.com/svn/trunk@10 b77917fb-863c-0410-9a43-030a88dac9d3

---
+++ libcurlssl.mod/src/ares/ares_timeout.c
@@ -0,0 +1,67 @@
+/* $Id: ares_timeout.c,v 1.9 2007-06-02 19:48:29 bagder Exp $ */
+
+/* Copyright 1998 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
+#include "setup.h"
+
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
+#include <time.h>
+
+#include "ares.h"
+#include "ares_private.h"
+
+struct timeval *ares_timeout(ares_channel channel, struct timeval *maxtv,
+                             struct timeval *tvbuf)
+{
+  struct query *query;
+  time_t now;
+  time_t offset, min_offset; /* these use time_t since some 32 bit systems
+                                still use 64 bit time_t! (like VS2005) */
+
+  /* No queries, no timeout (and no fetch of the current time). */
+  if (!channel->queries)
+    return maxtv;
+
+  /* Find the minimum timeout for the current set of queries. */
+  time(&now);
+  min_offset = -1;
+  for (query = channel->queries; query; query = query->next)
+    {
+      if (query->timeout == 0)
+        continue;
+      offset = query->timeout - now;
+      if (offset < 0)
+        offset = 0;
+      if (min_offset == -1 || offset < min_offset)
+        min_offset = offset;
+    }
+
+  /* If we found a minimum timeout and it's sooner than the one
+   * specified in maxtv (if any), return it.  Otherwise go with
+   * maxtv.
+   */
+  if (min_offset != -1 && (!maxtv || min_offset <= maxtv->tv_sec))
+    {
+      tvbuf->tv_sec = (long)min_offset;
+      tvbuf->tv_usec = 0;
+      return tvbuf;
+    }
+  else
+    return maxtv;
+}
---
+++ libcurlssl.mod/src/ares/ares_version.h
@@ -0,0 +1,25 @@
+/* $Id: ares_version.h,v 1.8 2007-05-30 21:45:56 bagder Exp $ */
+
+#ifndef ARES__VERSION_H
+#define ARES__VERSION_H
+
+#define ARES_VERSION_MAJOR 1
+#define ARES_VERSION_MINOR 4
+#define ARES_VERSION_PATCH 0
+#define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
+                       (ARES_VERSION_MINOR<<8)|\
+                       (ARES_VERSION_PATCH))
+#define ARES_VERSION_STR "1.4.0"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+const char *ares_version(int *version);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
+
