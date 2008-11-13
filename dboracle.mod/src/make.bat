01ef664b04e024097fe420b1da1c784981df38c5
woollybah <woollybah@b77917fb-863c-0410-9a43-030a88dac9d3>
2008-11-13 12:36:16 +0000

    Win32 updates.
    git-svn-id: http://maxmods.googlecode.com/svn/trunk@476 b77917fb-863c-0410-9a43-030a88dac9d3

--- dboracle.mod/src/make.bat
+++ dboracle.mod/src/make.bat
@@ -1,5 +1,9 @@
-SET ORACLE_INCLUDES=C:\stuff\instantclient-sdk-win32-11.1.0.6.0\instantclient_11_1\sdk\include
-SET ORACLE_LIBS=C:\stuff\instantclient-basic-win32-11.1.0.6.0\instantclient_11_1
+Rem DLL build script form Win32
+Rem 
+Rem 
+Rem SET ORACLE_INCLUDES=C:\stuff\instantclient-sdk-win32-11.1.0.6.0\instantclient_11_1\sdk\include
+Rem SET ORACLE_LIBS=C:\stuff\instantclient-basic-win32-11.1.0.6.0\instantclient_11_1
+SET ORACLE_INCLUDES=C:\000_programming\c\oracle\instantclient_11_1\sdk\include
+SET ORACLE_LIBS=C:\000_programming\c\oracle\instantclient_11_1
 
-gcc -shared -I. -I%ORACLE_INCLUDES% -L%ORACLE_LIBS% -DOCI_CHARSET_MIXED -loci ocilib.c main.c -o ..\lib\win32\ocilibm.dll
-
+gcc -shared -I. -I..\include -I%ORACLE_INCLUDES% -L%ORACLE_LIBS% -DOCI_CHARSET_MIXED -loci callback.c collection.c column.c connection.c connpool.c date.c define.c element.c error.c exception.c file.c format.c handle.c hash.c interval.c iterator.c library.c list.c lob.c long.c memory.c mutex.c number.c object.c resultset.c schema.c statement.c string.c thread.c threadkey.c timestamp.c transaction.c main.c -o ..\lib\win32\ocilibm.dll
