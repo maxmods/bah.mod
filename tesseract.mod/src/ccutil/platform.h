a14093acadbbf760be77524f999143d854d1c91b
woollybah <woollybah@b77917fb-863c-0410-9a43-030a88dac9d3>
2008-12-19 18:58:48 +0000

    BaH.tesseract. Initial Import (2)
    git-svn-id: http://maxmods.googlecode.com/svn/trunk@528 b77917fb-863c-0410-9a43-030a88dac9d3

---
+++ tesseract.mod/src/ccutil/platform.h
@@ -0,0 +1,26 @@
+// Place holder
+#define DLLSYM
+#ifdef __MSW32__
+// BaH
+#ifdef __GNUC__
+#define ultoa _ultoa
+typedef struct _BLOB {
+	unsigned int	cbSize;
+	char	*pBlobData;
+} BLOB,*LPBLOB;
+#endif
+#define SIGNED
+#define snprintf _snprintf
+#if (_MSC_VER <= 1400)
+#define vsnprintf _vsnprintf
+#endif
+#else
+#define __UNIX__
+#include <limits.h>
+#ifndef PATH_MAX
+#define MAX_PATH 4096
+#else
+#define MAX_PATH PATH_MAX
+#endif
+#define SIGNED signed
+#endif
