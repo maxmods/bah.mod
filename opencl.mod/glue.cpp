65a517c24550e175929ad17747dc18caddd088ab
woollybah <woollybah@b77917fb-863c-0410-9a43-030a88dac9d3>
2010-01-31 17:30:52 +0000

    Removed debug.
    git-svn-id: http://maxmods.googlecode.com/svn/trunk@988 b77917fb-863c-0410-9a43-030a88dac9d3

--- opencl.mod/glue.cpp
+++ opencl.mod/glue.cpp
@@ -337,10 +337,8 @@ BBArray * bmx_ocl_platform_getdevices(int deviceType) {
 	cl_device_id devices[10];
 
 	err = clGetDeviceIDs(NULL, deviceType, 10, devices, &size);
-printf("size = %d\n", size);fflush(stdout);
+
 	BBArray * list = _bah_opencl_TCLDevice__newDeviceList(size);
-printf("size = %d\n", size);fflush(stdout);
-printf("size = %d\n", size);fflush(stdout);
 	
 	for (int n = 0; n < size; n++) {
 		cl_device_id device = devices[n];
@@ -348,16 +346,11 @@ printf("size = %d\n", size);fflush(stdout);
 		size_t s = 0;
 	
 		err = clGetDeviceInfo(device, CL_DEVICE_TYPE, sizeof(type), &type, &s);
-printf("infosize = %d : %d\n", s, type);fflush(stdout);
 	
 		MaxCLDevice * dev = new MaxCLDevice(device);
-printf("A\n");fflush(stdout);
 		BBObject * obj = _bah_opencl_TCLDevice__setDevice(list, n, dev, type);
-printf("B\n");fflush(stdout);
 		dev->SetHandle(obj);
-printf("C\n");fflush(stdout);
 	}
-printf("D\n");fflush(stdout);
 	
 	return list;
 }
