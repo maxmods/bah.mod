e479a17ded39d3c05fc48fb9f8079e8b0cf3cddc
woollybah <woollybah@b77917fb-863c-0410-9a43-030a88dac9d3>
2013-06-29 13:18:35 +0000

    Update tiffconf.h for win32 build.
    git-svn-id: http://maxmods.googlecode.com/svn/trunk@1525 b77917fb-863c-0410-9a43-030a88dac9d3

--- magick.mod/src/delegates/libtiff/tiffconf.h
+++ magick.mod/src/delegates/libtiff/tiffconf.h
@@ -7,17 +7,20 @@
 #ifndef _TIFFCONF_
 #define _TIFFCONF_
 
-/* Define to 1 if the system has the type `int16'. */
-/* #undef HAVE_INT16 */
-
-/* Define to 1 if the system has the type `int32'. */
-/* #undef HAVE_INT32 */
-
-/* Define to 1 if the system has the type `int8'. */
-/* #undef HAVE_INT8 */
-
 /* The size of a `int', as computed by sizeof. */
 #define SIZEOF_INT 4
+
+/* The size of a `long', as computed by sizeof. */
+#include <limits.h>
+#if (LONG_MAX == +9223372036854775807L)
+#define SIZEOF_LONG 8
+#define SIZEOF_UNSIGNED_LONG 8
+#elif (LONG_MAX == +2147483647)
+#define SIZEOF_LONG 4
+#define SIZEOF_UNSIGNED_LONG 4
+#else
+#error "Cannot detect SIZEOF_LONG"
+#endif
 
 /* Signed 8-bit type */
 #define TIFF_INT8_T signed char
@@ -31,77 +34,31 @@
 /* Unsigned 16-bit type */
 #define TIFF_UINT16_T unsigned short
 
-/* Signed 32-bit type formatter */
-#define TIFF_INT32_FORMAT "%d"
-
 /* Signed 32-bit type */
 #define TIFF_INT32_T signed int
-
-/* Unsigned 32-bit type formatter */
-#define TIFF_UINT32_FORMAT "%u"
 
 /* Unsigned 32-bit type */
 #define TIFF_UINT32_T unsigned int
 
-#ifdef _WIN32
-
-
-/* Signed 64-bit type formatter */
-#define TIFF_INT64_FORMAT "%I64d"
+/* Signed / Unsigned 64-bit type */
+#ifdef _MSC_VER
+#define TIFF_INT64_T signed __int64
+#define TIFF_UINT64_T unsigned __int64
+#else
+#include <inttypes.h>
+#define TIFF_INT64_T int64_t
+#define TIFF_UINT64_T uint64_t
+#endif // _MSC_VER
 
 /* Signed 64-bit type */
-#define TIFF_INT64_T signed __int64
-
-/* Unsigned 64-bit type formatter */
-#define TIFF_UINT64_FORMAT "%I64u"
-
-/* Unsigned 64-bit type */
-#define TIFF_UINT64_T unsigned __int64
-
-/* Signed size type */
 #if defined(_WIN64)
 #define TIFF_SSIZE_T signed __int64
 #else
-#define TIFF_SSIZE_T signed int
-#endif
-
-/* Signed size type formatter */
-#if defined(_WIN64)
-#define TIFF_SSIZE_FORMAT "%I64d"
-#else
-#define TIFF_SSIZE_FORMAT "%ld"
-#endif
-
-/* Pointer difference type */
-#define TIFF_PTRDIFF_T long
-
-
-#else
-
-
-/* Signed 64-bit type formatter */
-#define TIFF_INT64_FORMAT "%lld"
-
-/* Signed 64-bit type */
-#define TIFF_INT64_T signed long long
-
-/* Unsigned 64-bit type formatter */
-#define TIFF_UINT64_FORMAT "%llu"
-
-/* Unsigned 64-bit type */
-#define TIFF_UINT64_T unsigned long long
-
-/* Signed size type */
 #define TIFF_SSIZE_T signed long
-
-/* Signed size type formatter */
-#define TIFF_SSIZE_FORMAT "%ld"
+#endif
 
 /* Pointer difference type */
 #define TIFF_PTRDIFF_T ptrdiff_t
-
-
-#endif
 
 /* Compatibility stuff. */
 
@@ -109,12 +66,35 @@
    machine */
 #define HAVE_IEEEFP 1
 
+/* 
+-----------------------------------------------------------------------
+Byte order
+-----------------------------------------------------------------------
+*/
+
+/*
+Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+significant byte first (like Motorola and SPARC, unlike Intel).
+Some versions of gcc may have BYTE_ORDER or __BYTE_ORDER defined
+If your big endian system isn't being detected, add an OS specific check
+*/
+#if (defined(BYTE_ORDER) && BYTE_ORDER==BIG_ENDIAN) || \
+	(defined(__BYTE_ORDER) && __BYTE_ORDER==__BIG_ENDIAN) || \
+	defined(__BIG_ENDIAN__)
+/* Set the native cpu bit order (FILLORDER_LSB2MSB or FILLORDER_MSB2LSB) */
+#define HOST_FILLORDER FILLORDER_MSB2LSB
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#define WORDS_BIGENDIAN 1
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#define HOST_BIGENDIAN 1
+#else
 /* Set the native cpu bit order (FILLORDER_LSB2MSB or FILLORDER_MSB2LSB) */
 #define HOST_FILLORDER FILLORDER_LSB2MSB
-
-/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian
-   (Intel) */
-#define HOST_BIGENDIAN 0
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#undef WORDS_BIGENDIAN
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#undef HOST_BIGENDIAN
+#endif // BYTE_ORDER
 
 /* Support CCITT Group 3 & 4 algorithms */
 #define CCITT_SUPPORT 1
@@ -136,7 +116,7 @@
 
 /* Support Old JPEG compresson (read contrib/ojpeg/README first! Compilation
    fails with unpatched IJG JPEG library) */
-/* #define OJPEG_SUPPORT 1 */
+/* #undef OJPEG_SUPPORT
 
 /* Support Macintosh PackBits algorithm */
 #define PACKBITS_SUPPORT 1
@@ -149,6 +129,12 @@
 
 /* Support Deflate compression */
 #define ZIP_SUPPORT 1
+
+/* Support LZMA2 compression */
+#undef LZMA_SUPPORT
+
+/* Support Microsoft Document Imaging format */
+#undef MDI_SUPPORT
 
 /* Support strip chopping (whether or not to convert single-strip uncompressed
    images to mutiple strips of ~8Kb to reduce memory usage) */
@@ -182,10 +168,3 @@
 #define IPTC_SUPPORT
 
 #endif /* _TIFFCONF_ */
-/*
- * Local Variables:
- * mode: c
- * c-basic-offset: 8
- * fill-column: 78
- * End:
- */
