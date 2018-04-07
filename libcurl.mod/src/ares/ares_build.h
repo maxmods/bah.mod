745231dca0d413056fd9cce0c88356e2eadf0d46
chtisgit <fdlr.christian@gmail.com>
2018-04-07 21:36:49 +0200

    Fix misplaced semicolon in bah.libcurl
    A semicolon in a #define statement prevented compilation of bah.libcurl on non-windows platforms

--- libcurl.mod/src/ares/ares_build.h
+++ libcurl.mod/src/ares/ares_build.h
@@ -203,7 +203,7 @@
 #    define CARES_TYPEOF_ARES_SSIZE_T long
 #  endif
 #else
-#  define CARES_TYPEOF_ARES_SSIZE_T ssize_t;
+#  define CARES_TYPEOF_ARES_SSIZE_T ssize_t
 #endif
 
 typedef CARES_TYPEOF_ARES_SSIZE_T ares_ssize_t;
